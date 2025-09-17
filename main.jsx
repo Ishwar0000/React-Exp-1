72990f30531c1160d7646130b9f460d30d05fad2
Ishwar0000 <118338769+Ishwar0000@users.noreply.github.com>
2025-09-17 15:11:22 +0530

    main.jsx

--- main.jsx
+++ main.jsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const ProductCard = ({ name, price, status }) => {
-  return (
-    <div className="product-card">
-      <h3>{name}</h3>
-      <p>Price: ${price}</p>
-      <p>Status: {status}</p>
-    </div>
-  );
-};
-
-export default ProductCard;
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
